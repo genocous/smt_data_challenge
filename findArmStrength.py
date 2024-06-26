07d0123bd7df2b00610eb469b21e51ae542e6d24
Andy Pan <andypan@umich.edu>
2024-06-26 10:27:58 +0200

    work on finding top throw speeds

--- findArmStrength.py
+++ findArmStrength.py
@@ -9,6 +9,10 @@ import numpy as np
 from SMT_data_starter import readDataSubset
 import matplotlib.pyplot as plt
 import pyarrow.dataset as pads
+from IPython.display import display
+
+#Courtesy of Eddie Dew's work in animation.py
+from animation import plot_animation
 
 
 #Uncomment the path for your local computer
@@ -19,7 +23,6 @@ def findSSthrows():
     temp = SSdf.iloc[1]
     ID = temp['Player_IDs']
     year = temp['Year']
-    level = temp['Level']
     if(year == 1883):   
         game_info_subset = readDataSubset('game_info', "/Users/andy/Desktop/2024_SMT_Data_Challenge/2024_SMT_Data_Challenge")
         game_info = game_info_subset.to_table(filter = (pads.field('Season') == "Season_1883")).to_pandas()
@@ -27,8 +30,9 @@ def findSSthrows():
         game_events_subset = readDataSubset('game_events', "/Users/andy/Desktop/2024_SMT_Data_Challenge/2024_SMT_Data_Challenge")
         game_events = game_events_subset.to_table(filter = (pads.field('Season') == "Season_1883")).to_pandas()
 
-        ball_pos_subset = readDataSubset('ball_pos', "/Users/andy/Desktop/2024_SMT_Data_Challenge/2024_SMT_Data_Challenge")
-        ball_pos = ball_pos_subset.to_table(filter = (pads.field('Season') == "Season_1883")).to_pandas()
+        player_position_subset = readDataSubset('player_pos', "/Users/andy/Desktop/2024_SMT_Data_Challenge/2024_SMT_Data_Challenge")
+        player_position_SS = player_position_subset.to_table(filter = (pads.field('Season') == "Season_1883") & (pads.field('player_position') == 6)).to_pandas()
+        player_position_1B = player_position_subset.to_table(filter = (pads.field('Season') == "Season_1883") & (pads.field('player_position') == 3)).to_pandas()
     else:
         game_info_subset = readDataSubset('game_info', "/Users/andy/Desktop/2024_SMT_Data_Challenge/2024_SMT_Data_Challenge")
         game_info = game_info_subset.to_table(filter = (pads.field('Season') == "Season_1884")).to_pandas()
@@ -36,8 +40,9 @@ def findSSthrows():
         game_events_subset = readDataSubset('game_events', "/Users/andy/Desktop/2024_SMT_Data_Challenge/2024_SMT_Data_Challenge")
         game_events = game_events_subset.to_table(filter = (pads.field('Season') == "Season_1884")).to_pandas()
 
-        ball_pos_subset = readDataSubset('ball_pos', "/Users/andy/Desktop/2024_SMT_Data_Challenge/2024_SMT_Data_Challenge")
-        ball_pos = ball_pos_subset.to_table(filter = (pads.field('Season') == "Season_1884")).to_pandas()
+        player_position_subset = readDataSubset('player_pos', "/Users/andy/Desktop/2024_SMT_Data_Challenge/2024_SMT_Data_Challenge")
+        player_position_SS = player_position_subset.to_table(filter = (pads.field('Season') == "Season_1884") & (pads.field('player_position') == 6)).to_pandas()
+        player_position_1B = player_position_subset.to_table(filter = (pads.field('Season') == "Season_1884") & (pads.field('player_position') == 3)).to_pandas()
 
     FirstBasecatches = game_events[(game_events['event_code'] == 2) & (game_events['player_position'] == 3)
                                      & (game_events.event_code.shift(1) == 3) & (game_events['player_position'].shift(1) == 6)]
@@ -53,10 +58,48 @@ def findSSthrows():
     PlayerCatchesWithTimestamps = PlayerCatchesWithTimestamps[["game_str", "play_id_x", "play_per_game", "start_time", "timestamp"]]
     PlayerCatchesWithTimestamps = PlayerCatchesWithTimestamps.rename(columns={'timestamp':'end_time', 'play_id_x':'play_id'})
     PlayerCatchesWithTimestamps['throw_time'] = (PlayerCatchesWithTimestamps['end_time'] - PlayerCatchesWithTimestamps['start_time']) / 1000
+    # PlayerCatchesWithTimestamps['x_position_SS'] = 0
+    # PlayerCatchesWithTimestamps['y_position_SS'] = 0
+    # PlayerCatchesWithTimestamps['x_position_1B'] = 0
+    # PlayerCatchesWithTimestamps['y_position_1B'] = 0
+    PlayerCatchesWithTimestamps['distance'] = 0
+    PlayerCatchesWithTimestamps['ft/sec'] = 0
+    PlayerCatchesWithTimestamps['mph'] = 0
+
+    pd.options.mode.chained_assignment = None
+    for play in range(len(PlayerCatchesWithTimestamps)):
+        print(play, "/", len(PlayerCatchesWithTimestamps))
+        SSpos = player_position_SS[(player_position_SS['game_str'] == PlayerCatchesWithTimestamps['game_str'].iloc[play]) &
+                                   (player_position_SS['play_id'] == PlayerCatchesWithTimestamps['play_id'].iloc[play]) &
+                                   (player_position_SS['player_position'] == 6) &
+                                   (player_position_SS['timestamp'] >= PlayerCatchesWithTimestamps['start_time'].iloc[play]) &
+                                   (player_position_SS['timestamp'] <= PlayerCatchesWithTimestamps['end_time'].iloc[play])]
+        pos1B = player_position_1B[(player_position_1B['game_str'] == PlayerCatchesWithTimestamps['game_str'].iloc[play]) &
+                                   (player_position_1B['play_id'] == PlayerCatchesWithTimestamps['play_id'].iloc[play]) &
+                                   (player_position_1B['player_position'] == 3) &
+                                   (player_position_1B['timestamp'] >= PlayerCatchesWithTimestamps['start_time'].iloc[play]) &
+                                   (player_position_1B['timestamp'] <= PlayerCatchesWithTimestamps['end_time'].iloc[play])]
+        # PlayerCatchesWithTimestamps.at[play, 'x_position_SS'] = SSpos['field_x'].iloc[0]
+        # PlayerCatchesWithTimestamps.at[play, 'y_position_SS'] = SSpos['field_y'].iloc[0]
+        # PlayerCatchesWithTimestamps.at[play, 'x_position_1B'] = pos1B['field_x'].iloc[0]
+        # PlayerCatchesWithTimestamps.at[play, 'y_position_1B'] = pos1B['field_y'].iloc[0]
+        distanceSSto1B = np.sqrt((SSpos['field_x'].iloc[0] - pos1B['field_x'].iloc[0])**2. + 
+                                (SSpos['field_y'].iloc[0] - pos1B['field_y'].iloc[0])**2.)
+        PlayerCatchesWithTimestamps['distance'].iloc[play] = distanceSSto1B
+        ftsec = distanceSSto1B / PlayerCatchesWithTimestamps['throw_time'].iloc[play]
+        PlayerCatchesWithTimestamps['ft/sec'].iloc[play] = ftsec
+        mph = ftsec / 1.467
+        PlayerCatchesWithTimestamps['mph'].iloc[play] = mph
+
     print(PlayerCatchesWithTimestamps)
-    PlayerCatchesWithDistance = "Hello World!"
-    print(PlayerCatchesWithDistance)
     
+    #animation check
+    # player_position_df = player_position_subset.to_table(filter = (pads.field('game_str') == "1883_011_Vis4AE_Home4A")).to_pandas()
+    # ball_position_subset = readDataSubset('ball_pos', "/Users/andy/Desktop/2024_SMT_Data_Challenge/2024_SMT_Data_Challenge")
+    # ball_position_df = ball_position_subset.to_table(filter = (pads.field('game_str') == "1883_011_Vis4AE_Home4A")).to_pandas()
+    # hi = plot_animation(player_position_df, ball_position_df, 3, False)
+    # with open("animation.html", "w") as file:
+    #     file.write(hi.data)
 
     # for index, row in SSdf.iterrows():
     #     ID = row['Player_IDs']
